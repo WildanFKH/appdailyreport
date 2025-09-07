782604cb07b840d3d62fa9eb35124a9d92dc111e
WildanFKH <128015108+WildanFKH@users.noreply.github.com>
2025-09-08 00:38:54 +0700

    Update db.js

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -2,8 +2,12 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log("✅ MongoDB Connected...");
+    await mongoose.connect(process.env.MONGO_URI, {
+      dbName: "dailyreport",
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("✅ MongoDB Connected to dailyreport...");
   } catch (error) {
     console.error("❌ Database connection error:", error);
     process.exit(1);
